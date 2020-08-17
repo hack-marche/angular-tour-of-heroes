524713ea7bd2ee2addc66bd416ff65ac9b018093
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-08-17 21:43:28 +0900

    tutorial 1-2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tour-of-heroes'`, () => {
+  it('should have as title \'angular-tour-of-heroes\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-tour-of-heroes');
