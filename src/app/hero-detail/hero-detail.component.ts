5239fcc2220aa29de3485e2a9f9489beb366e942
Naotsugu Someya <chome.au.lait.github@gmail.com>
2020-08-23 11:24:08 +0900

    ルーティングを使ったアプリ内ナビゲーションの追加

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Location } from '@angular/common'
 import Hero from '../types/hero'
+import { HeroService } from '~/app/heroes/hero.service'
+// import { MessageService } from '~/app/messages/message.service'
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,7 +12,22 @@ import Hero from '../types/hero'
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero()
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero))
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import Hero from '~/app/types/hero'
 import { HeroService } from '~/app/heroes/hero.service'
-import { MessageService } from '~/app/messages/message.service'
+// import { MessageService } from '~/app/messages/message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,27 +9,15 @@ import { MessageService } from '~/app/messages/message.service'
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
   heroes: Hero[]
-  selectedHero: Hero
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
     this.getHeroes()
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes))
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 }
